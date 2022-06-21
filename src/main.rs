d13facb9e2c2658a12dd624717e70591e11d7726
PatMan10 <PatMan10@protonmail.com>
2022-06-21 16:30:18 +0200

    done with chapter 2 of the book

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use rand::thread_rng;
 use rand::Rng;
+use std::cmp::Ordering;
 use std::io::stdin;
 
 fn main() {
@@ -8,10 +9,28 @@ fn main() {
     println!("It's between 1 and 10.");
     println!("Can you guess what it is?");
 
-    let secret_number = thread_rng().gen_range(1..11);
-    let mut guess = String::new();
-    stdin().read_line(&mut guess).expect("Failed to read line");
+    let rand_number = thread_rng().gen_range(1..11);
 
-    println!("\nYou guessed {}", guess);
-    println!("The secret number is {}", secret_number);
+    loop {
+        println!("Enter your guess: ");
+
+        let mut guess = String::new();
+
+        stdin().read_line(&mut guess).expect("Failed to read line");
+
+        let guess: i32 = match guess.trim().parse() {
+            Ok(n) => n,
+            Err(_) => continue,
+        };
+
+        match guess.cmp(&rand_number) {
+            Ordering::Less => println!("Too small!\n"),
+            Ordering::Greater => println!("Too big!\n"),
+            Ordering::Equal => {
+                println!("You guessed it!");
+                println!("The secret number is {}", rand_number);
+                break;
+            }
+        }
+    }
 }
